285770e956af77f5045066e68d90a311e8fd248d
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-03-19 22:55:21 +0000

    Refactored KVMap ; Removed KVArray

--- server/src/main/java/com/zenz/kvstore/server/KVMap.java
+++ server/src/main/java/com/zenz/kvstore/server/KVMap.java
@@ -1,141 +1,196 @@
 package com.zenz.kvstore.server;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.function.Consumer;
 
 public class KVMap {
-    public static int INITIAL_CAPACITY = 1000;
     public static float LOAD_FACTOR = 1.0f;
     public static int REHASH_BUCKETS = 100;
-    private int capacity;
+    private final float loadFactor;
+    private final int rehashBuckets;
     private KVArray ht1;
     private KVArray ht2;
-    private int rehashIdx;
+    private int rehashIdx = -1;
 
     public KVMap() {
-        capacity = INITIAL_CAPACITY;
-        ht1 = new KVArray(capacity);
-        rehashIdx = -1;
+        this(LOAD_FACTOR, REHASH_BUCKETS);
     }
 
-    public Node get(String key) {
-        int hashCode = key.hashCode();
-        int idx;
-
-        if (checkIsRehashing()) {
-            rehash();
-            idx = bucketIndex(hashCode, ht2.length());
-            NodeList nodeList = ht2.get(idx);
-            if (nodeList != null) {
-                return find(key, nodeList);
-            }
-        }
-
-        idx = bucketIndex(hashCode, ht1.length());
-        NodeList nodeList = ht1.get(idx);
-        if (nodeList != null) {
-            return find(key, nodeList);
-        }
-
-        return null;
+    public KVMap(float loadFactor, int rehashBuckets) {
+        this.loadFactor = loadFactor;
+        this.rehashBuckets = rehashBuckets;
+        ht1 = new KVArray();
     }
 
     public void put(String key, byte[] value) {
         int hashCode = key.hashCode();
-        int idx;
-        NodeList nodeList;
 
-        if (checkIsRehashing()) {
+        if (isRehashing()) {
+            rehash();
+
+            // Inserting into ht1
+            int idx = bucketIndex(hashCode, ht1.length());
+            NodeList nl = ht1.get(idx);
+            if (nl != null) {
+                Node node = findNode(key, nl);
+                if (node != null) {
+                    node.value = value;
+                } else {
+                    nl.add(new Node(key, value));
+                }
+                return;
+            }
+
+            // Inserting into ht2
             idx = bucketIndex(hashCode, ht2.length());
-            nodeList = ht2.get(idx);
-            if (nodeList == null) {
-                ht2.add(idx, new NodeList(new Node(key, value)));
-                return;
+            nl = ht2.get(idx);
+            if (nl == null) {
+                nl = new NodeList(ht2);
+                ht2.set(idx, nl);
+            }
+
+            Node node = findNode(key, nl);
+            if (node != null) {
+                node.value = value;
+            } else {
+                nl.add(new Node(key, value));
             }
         } else {
-            idx = bucketIndex(hashCode, ht1.length());
-            nodeList = ht1.get(idx);
-            if (nodeList == null) {
-                ht1.add(idx, new NodeList(new Node(key, value)));
-                return;
+            // Inserting into ht1
+            int idx = bucketIndex(hashCode, ht1.length());
+            NodeList nl = ht1.get(idx);
+            if (nl == null) {
+                nl = new NodeList(ht1);
+                ht1.set(idx, nl);
             }
-        }
-        
-        Node node = find(key, nodeList);
-        if (node != null) {
-            node.value = value;
-        } else {
-            nodeList.add(new Node(key, value));
+
+            Node node = findNode(key, nl);
+            if (node != null) {
+                node.value = value;
+            } else {
+                nl.add(new Node(key, value));
+            }
         }
     }
 
-    private Node find(String key, NodeList nodeList) {
-        if (nodeList == null) return null;
-        Node cur = nodeList.head;
+    public Node get(String key) {
+        int hashCode = key.hashCode();
 
-        while (cur != null) {
-            if (cur.key.equals(key)) {
-                return cur;
+        if (isRehashing()) {
+            rehash();
+
+            // Checking ht1
+            int idx = bucketIndex(hashCode, ht1.length());
+            NodeList nl = ht1.get(idx);
+            if (nl != null) return findNode(key, nl);
+
+            // Checking ht2
+            idx = bucketIndex(hashCode, ht2.length());
+            nl = ht2.get(idx);
+            return nl == null ? null : findNode(key, nl);
+        }
+
+        int idx = bucketIndex(hashCode, ht1.length());
+        NodeList nl = ht1.get(idx);
+        return nl == null ? null : findNode(key, nl);
+    }
+
+    public boolean remove(String key) {
+        int hashCode = key.hashCode();
+
+        if (isRehashing()) {
+            rehash();
+
+            // Checking ht1
+            int idx = bucketIndex(hashCode, ht1.length());
+            NodeList nl = ht1.get(idx);
+            if (nl != null) {
+                Node node = findNode(key, nl);
+                if (node == null) return false;
+                nl.remove(node);
+                return true;
             }
-            cur = cur.next;
+
+            // Checking ht2
+            idx = bucketIndex(hashCode, ht2.length());
+            nl = ht2.get(idx);
+            if (nl == null) return false;
+
+            Node node = findNode(key, nl);
+            if (node == null) return false;
+            nl.remove(node);
+            return true;
+        }
+
+        // Checking ht1
+        int idx = bucketIndex(hashCode, ht1.length());
+        NodeList nl = ht1.get(idx);
+        if (nl == null) return false;
+
+        Node node = findNode(key, nl);
+        if (node == null) return false;
+        nl.remove(node);
+        return true;
+    }
+
+    private Node findNode(String key, NodeList nodeList) {
+        if (nodeList == null) return null;
+
+        Iterator<Node> it = nodeList.iterator();
+        while (it.hasNext()) {
+            Node node = it.next();
+            if (node.key.equals(key)) {
+                return node;
+            }
         }
 
         return null;
     }
 
-    /**
-     * Moves min(ht1.length, buckets) buckets from ht1 to ht2.
-     * Once the rehashIdx >= ht1.length the rehashIdx is set to -1.
-     */
-    public void rehash() {
-        for (int i = 0; i < Math.min(ht1.length(), REHASH_BUCKETS); i++) {
-            NodeList nodeList = ht1.get(rehashIdx);
-
-            if (nodeList != null) {
-                String key = nodeList.head.key;
-                int newHashIdx = bucketIndex(key.hashCode(), ht2.length());
-                ht2.add(newHashIdx, nodeList);
-            }
-
-            rehashIdx += 1;
-        }
-
+    public boolean isRehashing() {
         if (rehashIdx >= ht1.length()) {
-            rehashIdx = -1;
             ht1 = ht2;
             ht2 = null;
-        }
-    }
-
-    /**
-     * Returns a non-negative bucket index.
-     * Note: Forcing the sign bit to 0.
-     */
-    private int bucketIndex(int hashCode, int len) {
-        return (hashCode & 0x7fffffff) % len;
-    }
-
-    /**
-     * Checks if we're currently in the middle of rehashing.
-     * <ul>
-     *     <li>True: If rehash index >= 0 or ht1.size() >= capacity * load factor</li>
-     *     <li>False: Otherwise</li>
-     * </ul>
-     */
-    private boolean checkIsRehashing() {
-        if (rehashIdx >= 0) {
-            return true;
+            rehashIdx = -1;
+            return false;
         }
 
-        if (ht1.size() >= (int) (capacity * LOAD_FACTOR)) {
-            capacity *= capacity;
-            ht2 = new KVArray(capacity);
+        if (rehashIdx >= 0) return true;
+
+        final int capacity = ht1.length();
+        if (ht1.size() >= (int) (capacity * loadFactor)) {
+            ht2 = new KVArray(capacity * capacity);
             rehashIdx = 0;
             return true;
         }
 
         return false;
     }
+
+    public void rehash() {
+        for (int i = 0; i < Math.min(ht1.length(), rehashBuckets); i++) {
+            NodeList nodeList = ht1.get(rehashIdx);
+
+            if (nodeList != null) {
+                String key = nodeList.head.key;
+                int bucketIndex = bucketIndex(key.hashCode(), ht2.length());
+                NodeList nl = ht2.get(bucketIndex);
+                if (nl != null) nl.merge(nodeList);
+                else ht2.set(bucketIndex, nodeList);
+            }
+
+            rehashIdx += 1;
+        }
+    }
+
+    /**
+     * Returns a non-negative bucket index.
+     */
+    private int bucketIndex(int hashCode, int len) {
+        return (hashCode & 0x7fffffff) % len;
+    }
+
 
     public KVArray getHt1() {
         return ht1;
@@ -145,18 +200,35 @@ public class KVMap {
         return ht2;
     }
 
+    public int capacity() {
+        return isRehashing() ? ht2.length() : ht1.length();
+    }
+
+    public float loadFactor() {
+        return loadFactor;
+    }
+
+    public int size() {
+        return isRehashing() ? ht2.size() + ht1.size() : ht1.size();
+    }
+
     public static class Node {
-        public String key;
-        public byte[] value;
-        public Node prev;
-        public Node next;
+        private final String key;
+        private byte[] value;
+        private Node prev;
+        private Node next;
 
-        public Node() {
-        }
-
-        public Node(String key, byte[] value) {
+        private Node(String key, byte[] value) {
             this.key = key;
             this.value = value;
+        }
+
+        public String key() {
+            return key;
+        }
+
+        public byte[] value() {
+            return value;
         }
 
         @Override
@@ -167,33 +239,53 @@ public class KVMap {
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (!(obj instanceof Node other)) return false;
-            return Objects.equals(this.key, other.key);
+            if (!(obj instanceof KVMap.Node other)) return false;
+            return Objects.equals(key, other.key);
         }
     }
 
-    public static class NodeList {
-        public Node head;
-        public Node tail;
+    public static class NodeList implements Iterable<Node> {
+        private Node head;
+        private Node tail;
+        private int size;
+        private final KVArray array;
 
-        public NodeList() {
+        public NodeList(KVArray array) {
+            this.array = array;
         }
 
-        public NodeList(Node node) {
-            head = node;
-            tail = node;
-        }
-
+        /**
+         * Appends the node to the tail of the list
+         *
+         * @param node
+         */
         public void add(Node node) {
             if (head == null) {
                 head = node;
                 tail = node;
-                return;
+            } else {
+                tail.next = node;
+                node.prev = tail;
+                tail = node;
             }
 
-            tail.next = node;
-            node.prev = tail;
-            tail = node;
+            size++;
+            array.size++;
+        }
+
+        /**
+         * Prepends the nodes in other NodeList into this list
+         * and increments size of this NodeList and the parent KVArray.
+         *
+         * @param other
+         */
+        public void merge(NodeList other) {
+            other.tail.next = head;
+            head.prev = other.tail;
+            head = other.head;
+
+            array.size += other.size;
+            size += other.size;
         }
 
         public void remove(Node node) {
@@ -209,15 +301,141 @@ public class KVMap {
                 head = node.next;
             }
 
+            if (node == tail) {
+                tail = node.prev;
+            }
+
             node.prev = null;
             node.next = null;
+
+            size++;
+            array.size--;
         }
 
-        public void forEach(Consumer<Node> consumer) {
+        public int size() {
+            return size;
+        }
+
+        public Iterator<Node> iterator() {
             Node cur = head;
             while (cur != null) {
-                consumer.accept(cur);
                 cur = cur.next;
+            }
+            return new NodeIterator(head, this);
+        }
+    }
+
+    private static class NodeIterator implements Iterator<Node> {
+        private Node cur;
+        private Node next;
+        private final NodeList nodeList;
+
+        private NodeIterator(Node cur, NodeList nodeList) {
+            this.next = cur;
+            this.nodeList = nodeList;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public Node next() {
+            cur = next;
+            next = cur.next;
+            return cur;
+        }
+
+        @Override
+        public void remove() {
+            nodeList.remove(cur);
+        }
+    }
+
+    public class KVArray implements Iterable<NodeList> {
+        public static final int INITIAL_CAPACITY = 1000;
+        private final int capacity;
+        private int size;
+        private final NodeList[] arr;
+
+        private KVArray() {
+            this(INITIAL_CAPACITY);
+        }
+
+        private KVArray(int initialCapacity) {
+            this.capacity = initialCapacity;
+            this.arr = new NodeList[this.capacity];
+        }
+
+        public NodeList get(int index) {
+            if (index < 0 || index >= arr.length) {
+                return null;
+            }
+
+            return arr[index];
+        }
+
+        public void set(int index, NodeList value) {
+            if (index < 0 || index >= arr.length) {
+                throw new ArrayIndexOutOfBoundsException("Index: " + index + ", Size: " + arr.length);
+            }
+
+            arr[index] = value;
+        }
+
+        @Override
+        public Iterator<NodeList> iterator() {
+            return new NodeListIterator();
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public int length() {
+            return capacity;
+        }
+
+        private class NodeListIterator implements Iterator<NodeList> {
+            private int index = 0;
+
+            private NodeListIterator() {
+            }
+
+            @Override
+            public boolean hasNext() {
+                index = nextIndex(index);
+                return index != -1;
+            }
+
+            @Override
+            public NodeList next() {
+                if (index == -1) {
+                    throw new NoSuchElementException();
+                }
+
+                NodeList nodeList = arr[index];
+                index++;
+                return nodeList;
+            }
+
+            /**
+             * @param start Start index
+             * @return Returns the next index with a non-null value.
+             * Else -1 if it couldn't be found.
+             */
+            private int nextIndex(int start) {
+                for (int i = start; i < arr.length; i++) {
+                    if (arr[i] != null) return i;
+                }
+
+                return -1;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
             }
         }
     }
